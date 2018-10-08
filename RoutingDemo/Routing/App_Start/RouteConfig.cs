158519ce4046a271a762da31750bba62995c5e66
sefadogan <sefadogan7@gmail.com>
2018-10-09 00:47:06 +0300

    Routing codes have been added.
    Added routing codes for homepage, about and contact pages. Also added links on the frontend.

--- RoutingDemo/Routing/App_Start/RouteConfig.cs
+++ RoutingDemo/Routing/App_Start/RouteConfig.cs
@@ -14,6 +14,24 @@ namespace Routing
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Contact",
+                url: "Contact",
+                defaults: new { controller = "Home", action = "Contact" }
+            );
+
+            routes.MapRoute(
+                name: "About",
+                url: "About",
+                defaults: new { controller = "Home", action = "About" }
+            );
+
+            routes.MapRoute(
+                name: "Home",
+                url: "Home",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
